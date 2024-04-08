5d595127c125bafbb6ac19e4649b0d327307659b
Barış Uluer <baris.uluer@gmail.com>
2024-04-09 01:31:20 +0300

    Half Way

---
+++ main.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import cryptocode
+
+window = Tk()
+window.title("Secret Notes")
+window.config(padx=30, pady=30)
+
+
+def save_encrypt():
+    title = title_entry.get()
+    our_file = open("secret.txt", "a")
+    note = secret_text.get("1.0", END)
+    encoded = cryptocode.encrypt(message=note, password=master_key_entry.get())
+    our_file.write(f"{title}\n{encoded}\n")
+    our_file.close()
+    title_entry.delete(0, END)
+    secret_text.delete("1.0", END)
+    master_key_entry.delete(0, END)
+
+
+def decrypt_function():
+    try:
+        decoded = cryptocode.decrypt(secret_text.get("1.0", END), password=master_key_entry.get())
+        secret_text.delete("1.0", END)
+        secret_text.insert(INSERT, decoded)
+    except:
+        pass
+
+
+# Image Part
+img = ImageTk.PhotoImage(Image.open("secret-100.png"))
+panel = Label(window, image=img, )
+panel.pack()
+
+# Text Parts
+title_label = Label(text="Enter Your Title", font=("Arial", 13))
+title_label.pack()
+
+title_entry = Entry(width=45)
+title_entry.pack()
+
+secret_label = Label(text="Enter Your Secret", font=("Arial", 13))
+secret_label.pack()
+
+secret_text = Text(width=44, height=24)
+secret_text.pack()
+
+master_key_label = Label(text="Enter Master Key", font=("Arial", 13))
+master_key_label.pack()
+
+master_key_entry = Entry(width=45)
+master_key_entry.pack()
+
+save_button = Button(text="Save & Encrypt", command=save_encrypt)
+save_button.pack()
+
+decrypt_button = Button(text="Decrypt", command=decrypt_function)
+decrypt_button.pack()
+
+window.mainloop()
